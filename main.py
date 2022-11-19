181700879f50c5a79632f9c8bd8b86554b994ec8
Sushant.sinha_94 <sushant1058jan@gmail.com>
2022-11-19 21:50:00 +0530

    added enemy

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ icon=pygame.image.load('spaceshipIcon/i64.png')
 pygame.display.set_icon(icon)
 
 
-
+#Enemy
+enemyImg = pygame.image.load('spaceshipIcon/enemy.png')
+enemy_x=370
+enemy_y=50
 
 
 #player
@@ -20,9 +23,14 @@ playerImg = pygame.image.load('spaceshipIcon/i64.png')
 player_x=370
 player_y=480
 
+player_x_change=0
 
-def player():
-    screen.blit(playerImg,(player_x,player_y))
+
+def player(x,y):
+    screen.blit(playerImg,(x,y))
+    
+def enemy(x,y):
+    screen.blit(enemyImg,(x,y))
 
 #Game loop
 running=True 
@@ -33,7 +41,23 @@ while running:
         if event.type==pygame.QUIT:  #quit when close button is clicked
             running=False
             
+        #if key is pressed check wheather it is right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                player_x_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
             
-    player()
+    player_x += player_x_change
+    
+    if player_x < 0:
+        player_x =0
+    elif player_x >= 736:
+        player_x = 736
+    player(player_x,player_y)
+    enemy(enemy_x,enemy_y)
     pygame.display.update()
     
